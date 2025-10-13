d360077206d81bc7b6828d81e930022090edade3
Aditya Adarsh <aditya18adarsh@gmail.com>
2025-10-13 14:47:48 +0530

    dom.js

---
+++ Javascript/ES6/dom.js
@@ -0,0 +1,6 @@
+const div=document.createElement("div");
+const h1=document.createElement("h1");
+h1.innerText="Hello Coding World";
+const id=document.getElementById("root");
+id.appendChild(div);
+div.appendChild(h1);
